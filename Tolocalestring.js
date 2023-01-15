417d48673892b1710b5c53127cb69584a17969f8
chiranjeev kajode <98865769+chiranjeevkajode@users.noreply.github.com>
2023-01-15 21:46:40 +0530

    JavaScript

---
+++ Tolocalestring.js
@@ -0,0 +1,14 @@
+// const date1 = new Date(Date.UTC(2012, 11, 20, 3, 0, 0));
+
+// console.log(date1.toLocaleString('ar-EG'));
+// const number1 = 123456.789;
+
+// console.log(number1.toLocaleString());
+
+const obj = {
+    toString() {
+        return "Chiranjeev";
+    }
+};
+console.log(obj.toLocaleString());
+
