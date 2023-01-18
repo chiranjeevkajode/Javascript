f4a96750707c8dfe096fca6b35c48159d6fdcab8
chiranjeev kajode <98865769+chiranjeevkajode@users.noreply.github.com>
2023-01-18 23:28:25 +0530

    JavaScript

---
+++ hasOwn.js
@@ -0,0 +1,9 @@
+const obj = {
+    prop: 'exists'
+  };
+  
+  console.log(Object.hasOwn(obj, 'prop'));
+  
+  console.log(Object.hasOwn(obj, 'prop'));
+  
+  console.log(Object.hasOwn(obj, 'How are you'));
