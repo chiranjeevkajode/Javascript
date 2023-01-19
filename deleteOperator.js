1e3288d727bb1550ad2b8d39b809e76b49756357
chiranjeev kajode <98865769+chiranjeevkajode@users.noreply.github.com>
2023-01-19 21:40:07 +0530

    JavaScript

---
+++ deleteOperator.js
@@ -0,0 +1,12 @@
+const Student = {
+    firstname: 'Mohan',
+    lastName: 'Pandey',
+    age:17
+  };
+  
+  console.log(Student.firstname);
+  
+  delete Student.firstname;
+  
+  console.log(Student.firstname);
+  console.log(Student.age);
